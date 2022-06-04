96f46b3f79391fec7d200b3db437e30092416a00
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-06-04 12:45:31 +0200

    Upravy formulare

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,110 +1,163 @@
 //import { useState } from 'react/cjs/react.production.min';
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './style.css';
-import DatePicker from 'react-date-picker';
-
+import { Formik } from 'formik';
+import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
+import { AuctionDateTimePicker } from '../fields/AuctionDateTimePicker';
 export const Form = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [value, onChange] = useState([new Date(), new Date()]);
   return (
     <>
-      <h2>Založení dobročinné aukce</h2>
-      <form
-        onSubmit={() => {
-          console.log('Test');
-        }}
-      >
-        <label>
-          Zakladatel
-          <div>
-            <label>
-              Jméno
-              <input id="jmeno-prijmeni" type="text" name="auctionName" />
-            </label>
-            <label>
-              Příjmení
-              <input id="jmeno-prijmeni" type="text" name="auctionName" />
-            </label>
-          </div>
-        </label>
-        <label>
-          Aukce
-          <div>
-            <label>
-              Název aukce
-              <select>
-                <option value="first">First</option>
-                <option value="second">Second</option>
-                <option value="third">Third</option>
-              </select>
+      <div>
+        <h2>Založení dobročinné aukce</h2>
+        <Formik
+          initialValues={{
+            jmeno: '',
+            prijmeni: '',
+            nazev: '',
+            popis: '',
+            problem: '',
+            pouziti: '',
+            souhlas: false,
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form className="form" onSubmit={handleSubmit}>
+              <label className="form__zakladatel">Zakladatel</label>
+              <label className="form__jmeno-label" for="jmeno">
+                Jméno
+              </label>
+              <input
+                className="form__jmeno-input"
+                id="jmeno"
+                type="text"
+                name="jmeno"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.jmeno}
+              />
+              <label className="form__prijmeni-label" for="prijmeni">
+                Příjmení{' '}
+              </label>
+              <input
+                className="form__prijmeni-input"
+                id="prijmeni"
+                type="text"
+                name="prijmeni"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.prijmeni}
+              />
+              <label className="form__aukce">Aukce</label>
+              <label className="form__nazev-label" for="nazev">
+                Název aukce
+              </label>
+              <input
+                className="form__nazev-input"
+                id="nazev"
+                type="text"
+                name="nazev"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.nazev}
+              />
               {/* <input type="text" name="auctionName" /> */}
-            </label>
-          </div>
-        </label>
-        <label>
-          Předmět dražby
-          <input
-            className="chooseFile"
-            type="file"
-            placeholder="Nahrát fotografie"
-          />
-        </label>
-        <label>
-          Znalecký posudek
-          <input className="chooseFile" type="file" />
-        </label>
-        <label id="explanation">
-          Popis předmětu
-          <input
-            id="description"
-            type="text"
-            name="auctionName"
-            placeholder="O jaký předmět se jedná? Jaké je jeho stáří? Jedná se o originál?"
-          />
-        </label>
-        <label id="explanation">
-          Komu pomůžeme
-          <input
-            id="description"
-            type="text"
-            name="auctionName"
-            placeholder="Zde se, prosím, více rozepište.  O jaký problém se jedná? Jaký byl vývoj a jaký je stav nyní?"
-          />
-          <input type="file" />
-        </label>
-        <label id="explanation">
-          Výtěžek z aukce
-          <input
-            id="description"
-            type="text"
-            name="auctionName"
-            placeholder="Jak konkrétně bude výtěžek z aukce použit? V čem to obdarovanému pomůže?"
-          />
-        </label>
-        <label>
-          Doba trvání aukce
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-        </label>
-        <label id="checkbox">
-          <p>
-            Souhlasím se zpracováním osobních údajů a s obchodními podmínkami
-            ProDobro
-          </p>
-
-          {/* Checkbox to submit information */}
-          <input type="checkbox" />
-        </label>
-        {/* <input type="submit" value="submit" /> */}
-
-        <Link to="/Auction">
-          <button className="home__btn"> Založit aukci</button>
-        </Link>
-      </form>
+              <label className="form__predmet-label" for="predmet">
+                Předmět dražby{' '}
+              </label>
+              <input
+                className="form__predmet-input"
+                id="predmet"
+                type="file"
+                placeholder="Nahrát fotografie"
+              />
+              <label className="form__posudek-label" for="posudek">
+                Znalecký posudek
+              </label>
+              <input className="form__popis-input" id="posudek" type="file" />
+              <label className="form__popis-label" for="popis">
+                Popis předmětu{' '}
+              </label>
+              <input
+                className="form__popis-input"
+                id="popis"
+                type="text"
+                name="popis"
+                placeholder="O jaký předmět se jedná? Jaké je jeho stáří? Jedná se o originál?"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.popis}
+              />
+              <label className="form__problem-label" for="problem">
+                Komu{' '}
+              </label>
+              <input
+                className="form__problem-input"
+                id="problem"
+                type="text"
+                name="problem"
+                placeholder="Zde se, prosím, více rozepište.  O jaký problém se jedná? Jaký byl vývoj a jaký je stav nyní?"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.problem}
+              />
+              <div />
+              <label className="form__pouziti-label" for="pouziti">
+                Výtěžek z aukce
+              </label>
+              <input
+                className="form__pouziti-input"
+                id="pouziti"
+                type="text"
+                name="pouziti"
+                placeholder="Jak konkrétně bude výtěžek z aukce použit? V čem to obdarovanému pomůže?"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.pouziti}
+              />
+              <label className="form__timer-label" for="timer">
+                Doba trvání aukce
+              </label>
+              <div className="form__timer-picker">
+                <AuctionDateTimePicker name={'cas'} />
+              </div>
+              <label className="form__souhlas-label" for="souhlas">
+                Souhlasím se zpracováním osobních údajů a s obchodními
+                podmínkami ProDobro{' '}
+              </label>
+              {/* Checkbox to submit information */}
+              <input
+                className="form__souhlas-input"
+                id="souhlas"
+                name="souhlas"
+                type="checkbox"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                checked={values.souhlas}
+              />
+              {/* <input type="submit" value="submit" /> */}
+              <button className="home__btn"> Založit aukci</button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </>
   );
 };
